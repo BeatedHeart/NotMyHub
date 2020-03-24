946eebd303d045db64f146f28d2af6036c047281
BeatedHeart <59542950+BeatedHeart@users.noreply.github.com>
2020-03-23 17:36:24 -0700

    Update Detector.lua

--- Detector.lua
+++ Detector.lua
@@ -1,14 +1,1 @@
-if getgenv().enter ~= "yea" then
-	while wait() do end
-end
-
-if game.PlaceId == 1991926880 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/NotMyHub/master/Scripts/Supervillain.lua"))()
-else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/TellMeWhyYouLikeToEatCheese-/master/Scripts/UndetectedMain"))
-	end
-if game.PlaceId == 606849621 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/NotMyHub/master/Scripts/Jailbreak.lua"))()
-else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/TellMeWhyYouLikeToEatCheese-/master/Scripts/UndetectedMain"))
-	end
+wrong
