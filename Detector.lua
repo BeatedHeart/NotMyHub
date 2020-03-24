00a34a69a41be6b874b08326d5026d035a1d2c41
BeatedHeart <59542950+BeatedHeart@users.noreply.github.com>
2020-03-23 17:47:11 -0700

    Update Detector.lua

--- Detector.lua
+++ Detector.lua
@@ -1,1 +1,7 @@
-wrong
+
+if game.PlaceId == 606849621 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/NotMyHub/master/Scripts/Jailbreak.lua"))()
+end
+if game.PlaceId ~= 606849621 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHeart/TellMeWhyYouLikeToEatCheese-/master/Scripts/UndetectedMain"))()
+end
